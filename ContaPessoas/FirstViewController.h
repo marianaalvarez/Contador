54a2f999b8691fbb97b1b9e177d523f0df855b70
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-02-19 08:57:59 -0200

    Alteração nos comentarios

--- ContaPessoas/FirstViewController.h
+++ ContaPessoas/FirstViewController.h
@@ -2,8 +2,8 @@
 //  FirstViewController.h
 //  ContaPessoas
 //
-//  Created by Vinicius Miana on 2/18/15.
-//  Copyright (c) 2015 Vinicius Miana. All rights reserved.
+//  Created by Mariana Alvarez on 2/18/15.
+//  Copyright (c) 2015 Mariana Alvarez. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
